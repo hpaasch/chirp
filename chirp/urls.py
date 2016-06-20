0a73bdb46e0304f43c433086cc6602babed26be6
hpaasch <hepaasch@gmail.com>
2016-06-20 11:44:46 -0400

    Templates and CSS added. IndexView started.

--- chirp/urls.py
+++ chirp/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from main import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.IndexView.as_view(), name='index_view'),
+
 ]
