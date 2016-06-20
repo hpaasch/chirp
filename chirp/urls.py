d6bd92f77c8a138ffc08518ace5c97217e1c30d0
hpaasch <hepaasch@gmail.com>
2016-06-20 12:32:35 -0400

    finish day one. user login created.

--- chirp/urls.py
+++ chirp/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include  # added import
 from django.contrib import admin
 
 from main import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url('^', include('django.contrib.auth.urls')),  # going to get url patterns and drop in. from django docs
     url(r'^$', views.IndexView.as_view(), name='index_view'),
 
 ]
