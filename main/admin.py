029aaac035f95437a6417f39d17fe584fa682fa6
hpaasch <hepaasch@gmail.com>
2016-06-21 10:51:25 -0400

    working on ListView

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from main.models import Chirp
+
+class ChirpAdmin(admin.ModelAdmin):
+    list_display = ['body', 'bird']
+    search_fields = ['body']  # allows search of the body field 
+
+
+admin.site.register(Chirp, ChirpAdmin)
--- main/views.py
+++ main/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.views.generic import TemplateView, ListView
 
+from main.models import Chirp
 
-class IndexView(TemplateView):
+# class IndexView(TemplateView):
+#     template_name = 'index.html'
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)  # boilerplate
+#         context['all_chirps'] = Chirp.objects.all()
+#         return context
+
+class IndexView(ListView):
     template_name = 'index.html'
-    
+    model = Chirp   # to here completely replicates what's above
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)  # boilerplate... ditch the args for super python3!!
+        context['amount'] = Chirp.objects.all().count()  # on-the-fly calculations can happen here
+        return context
