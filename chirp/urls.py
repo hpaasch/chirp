0987b592797662962002850438fc455636a10ec6
hpaasch <hepaasch@gmail.com>
2016-06-22 09:17:48 -0400

    update

--- chirp/urls.py
+++ chirp/urls.py
@@ -1,18 +1,4 @@
-"""chirp URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url, include  # added import
 from django.contrib import admin
 
@@ -22,5 +8,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('^', include('django.contrib.auth.urls')),  # going to get url patterns and drop in. from django docs
     url(r'^$', views.IndexView.as_view(), name='index_view'),
+    url(r'^chirp/(?P<pk>\d+)/$', views.ChirpDetailView.as_view(), name='chirp_detail_view'),
+    url(r'^create_chirp/$', views.ChirpCreateView.as_view(), name='chirp_create_view'),
 
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import CreateView  # note this import
 
 from main.models import Chirp
 
@@ -19,3 +20,24 @@ class IndexView(ListView):
         context = super().get_context_data(**kwargs)  # boilerplate... ditch the args for super python3!!
         context['amount'] = Chirp.objects.all().count()  # on-the-fly calculations can happen here
         return context
+
+
+class ChirpDetailView(DetailView):
+    # template_name = we can give it a name. OR we can do it the way django wants. created new template dir
+    model = Chirp  # this is all needed to show a complete detail view
+
+    def get_queryset(self):
+        return Chirp.objects.filter(bird=self.request.user)  # SECURITY CHECK means you can't see others. security issue.
+        # can deal with superadmin... 'if' check
+
+
+class ChirpCreateView(CreateView):
+    model = Chirp
+    fields = ['body']  # don't include bird, cuz then you can chirp for someone else
+    # at just this, it doesn't hook up to the user ForeignKey... do the form_valid
+    success_url = '/'
+
+    def form_valid(self, form):
+        chirp = form.save(commit=False)   #this half creates it.
+        chirp.bird = self.request.user
+        return super().form_valid(form)
